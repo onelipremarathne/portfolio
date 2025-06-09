c6c466642647fcc3cc1dd24a3952c5854952c9d8
onelipremarathne <ovpremarathne@students.nsbm.ac.lk>
2025-06-09 12:57:18 +0530

    feat: mobile optimizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ export default function Home() {
   return (
     <div className="relative">
       <div className="sticky top-0 h-screen bg-gradient-to-r from-pink-100 via-pink-200 to-pink-300">
-        <div className="flex items-center justify-center h-screen max-w-[80%] mx-auto">
-          <div className="text-7xl text-pink-950 w-1/2 text-right mr-10 pr-10 leading-25 border-r-2 border-pink-900">
+        <div className="flex flex-col sm:flex-col md:flex-row items-center justify-center h-screen md:max-w-[80%] mx-auto">
+          <div className="text-5xl sm:text-5xl md:text-7xl text-pink-950 w-1/2 text-center sm:text-center md:text-right mb-10 sm:mb-10 pb-10 sm:pb-10 md:mr-10 md:pr-10 md:leading-25 border-b-2 sm:border-b-2 md:border-r-2 md:border-b-0 border-pink-900">
             <div>Hi, I'm</div>Oneli Premarathne
           </div>
-          <div className="text-2xl text-pink-800 mt-4 w-1/2 text-left">
+          <div className="text-xl sm:text-xl md:text-2xl text-pink-800 w-1/2 textcenter sm:text-center md:text-left">
             I am a motivated, hardworking and an energetic person who has
             developed a mature and a responsible approach to any task that I am
             presented with. With the strong skills acquired through a range of
@@ -16,7 +16,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      
+
       <div className="sticky top-0 h-screen bg-gradient-to-r from-rose-100 via-rose-200 to-rose-300">
 
 
