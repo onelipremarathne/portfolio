8e10d2deb2ff91bf719d1c96ce14c9fe44a5ba54
onelipremarathne <ovpremarathne@students.nsbm.ac.lk>
2025-06-10 09:23:24 +0530

    feat: another unsuccessful attempt at getting GSAP working

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import { getUserProfile, getUserRepositories } from "@/services/github";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 interface Repository {
   name: string;
@@ -61,14 +65,24 @@ const Projects = () => {
     fetchData();
   }, []);
 
+  const container = useRef<HTMLDivElement>(null);
+  const { contextSafe } = useGSAP({ scope: container });
+
+  const onClickGood = contextSafe(() => {
+    gsap.to(".good", { rotation: "+=360" });
+  });
+
   if (loading) {
-    return <div className="text-2xl">Loading...</div>;
+    return <div className="flex flex-col items-center justify-center h-screen text-2xl">Loading...</div>;
   }
 
   const { repositories } = data;
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div
+      ref={container}
+      className="flex flex-col items-center justify-center h-screen"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
@@ -83,8 +97,9 @@ const Projects = () => {
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-4 sm:gap-4 md:gap-8">
         {repositories.map((item, index) => (
           <div
+            onClick={onClickGood}
             key={index}
-            className="group relative mx-auto w-full max-w-sm md:h-md rounded-2xl cursor-pointer 
+            className="good group relative mx-auto w-full max-w-sm md:h-md rounded-2xl cursor-pointer 
                        overflow-hidden bg-white/20 px-4 sm:px-6 pt-6 sm:pt-10 pb-6 sm:pb-8 
                        ring-1 ring-gray-900/5 transition-all duration-300 hover:-translate-y-1"
           >
@@ -117,7 +132,8 @@ const Projects = () => {
                     alt={`${item.language} icon`}
                     className="w-8 h-8 sm:w-8 sm:h-8 md:w-12 md:h-12"
                     onError={(e) => {
-                      e.currentTarget.src = "/icons/default.svg";
+                      e.currentTarget.src =
+                        "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg";
                     }}
                   />
                 </div>
